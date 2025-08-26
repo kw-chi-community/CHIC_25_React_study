13e77aa82ee40938abc0c0067d7e8e0dc957bd26
서현 <interstellar@seohyeon-ui-MacBookAir.local>
2025-08-26 21:57:52 +0900

    [psh] 파일 정리

---
+++ 박서현/everytime-gpa-clone/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="page">
+      <header className="header">
+        <div className="container">
+          <h1 style={{margin:0}}>학점 계산기 (Vite React 클론)</h1>
+        </div>
+      </header>
+
+      <main className="container">
+        <section className="section">
+          초기 레이아웃 준비됨
+        </section>
+      </main>
+    </div>
+  );
+}
