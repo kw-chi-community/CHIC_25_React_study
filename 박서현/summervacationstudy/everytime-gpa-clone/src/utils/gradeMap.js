92608a14332ddb4e4f8368c63522995fea3278f1
서현 <interstellar@seohyeon-ui-MacBookAir.local>
2025-08-26 17:02:53 +0900

    [psh] 학점 계산 기능 구현

---
+++ 박서현/summervacationstudy/everytime-gpa-clone/src/utils/gradeMap.js
@@ -0,0 +1,7 @@
+export const GRADE_POINTS = {
+  'A+': 4.5, 'A0': 4.0, 
+  'B+': 3.5, 'B0': 3.0, 
+  'C+': 2.5, 'C0': 2.0, 
+  'D+': 1.5, 'D0': 1.0, 
+};
+export const GRADE_OPTIONS = Object.keys(GRADE_POINTS);
